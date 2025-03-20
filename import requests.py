290df4363a3339d9ab800b2fb7d5529f8742fcfa
owseaman <esowoeye@gmail.com>
2025-03-19 18:16:48 -0600

    remove secrets

--- import requests.py
+++ import requests.py
@@ -4,11 +4,11 @@ import json
 # Define the headers as a Python dictionary
 headers = {
     'Content-type': 'application/json',
-    'X-Automation-Webhook-Token': '021458c07124f92e6a50860784d5b3f4daaeeb9e'
+    'X-Automation-Webhook-Token': '*' ### Add your webhook token here
 }
 
 # Define the webhook URL
-url = 'https://api-private.atlassian.com/automation/webhooks/jira/a/426b94c7-ccb3-4be5-b9a2-e9d7f68ca87f/0195aa58-cbbb-7e07-8bd3-be692afa8c6b'
+url = '*' ### Add your webhook URL here
 
 try:
     # Make the POST request
